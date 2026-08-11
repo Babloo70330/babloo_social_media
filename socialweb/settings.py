9557f51d20c0e4588836e6c62d8836f49bb7cd56
BABLOO KUMAR <babloo.kumar70330@gmail.com>
2026-08-11 15:31:53 +0530

    Update settings.py

--- socialweb/settings.py
+++ socialweb/settings.py
@@ -9,21 +9,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY",
     "django-development-secret-key"
 )
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     ".vercel.app",
 ]
-
 
 # Application definition
 
