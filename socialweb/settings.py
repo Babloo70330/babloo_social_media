815f353f5884906e5ea9910e4e0102d0714991c1
BABLOO KUMAR <babloo.kumar70330@gmail.com>
2026-08-11 16:58:33 +0530

    Update settings.py

--- socialweb/settings.py
+++ socialweb/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 
@@ -113,10 +114,12 @@ WSGI_APPLICATION = "socialweb.wsgi.application"
 # ============================================================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        env="DATABASE_URL_UNPOOLED",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
